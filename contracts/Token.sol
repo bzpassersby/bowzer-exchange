ba6c6abcaa20f64fa6618946ee18db66aea2a05a
Bowen Zhang <bowenzhang@Bowens-MacBook-Pro.local>
2022-10-22 13:17:38 -0400

    2.Smoke Test

---
+++ contracts/Token.sol
@@ -0,0 +1,12 @@
+//SPDX_License_Identifer: unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+
+   string public name = "My Token";
+
+}
+
+
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,5 +3,11 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: "0.8.9",
+  network: {
+localhost: {}
+
+
+  }
+}
+
