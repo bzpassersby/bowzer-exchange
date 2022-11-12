0c762a65da08dbd9fc15145a06661f0e1b2aa58c
Bowen Zhang <bowenzhang@Bowens-MacBook-Pro.local>
2022-11-12 11:31:47 -0500

    5.Token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,11 +10,48 @@ contract Token {
    uint256 public decimals= 18;  
    uint256 public totalSupply;
 
+   //Track Balances
+   mapping(address => uint256) public balanceOf;
+   // Send Tokens
+
+
+   event Transfer(
+      address indexed from, 
+      address indexed to, 
+      uint256 value
+      );
+
+
    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
    	name=_name;
    	symbol=_symbol;
    	totalSupply=_totalSupply*(10**decimals);
+   	balanceOf[msg.sender]=totalSupply;
    }
+
+   function transfer(address _to, uint256 _value) 
+      public 
+      returns (bool success) 
+   {
+      //Require that sender has sufficient funds
+      require(balanceOf[msg.sender]>_value);
+      //Require receiver has a valid address
+      require(_to != address(0),"invalid receiver address");
+
+
+      // Deduct tokens from spender
+      balanceOf[msg.sender]=balanceOf[msg.sender] - _value;
+      // Credit tokens to receiver
+      balanceOf[_to]=balanceOf[_to]+ _value;
+
+      //Emit Event
+      emit Transfer(msg.sender, _to, _value);
+
+      return true;
+
+   }
+
+
 }
 
 
--- test/Token.js
+++ test/Token.js
@@ -6,13 +6,19 @@ const tokens=(n)=>{
 
 describe('Token',()=>{
 	//Tests go inside here
-let token
+let token,accounts,deployer, receiver
 
 
 
 beforeEach(async ()=>{
 	const Token=await ethers.getContractFactory('Token')
     token= await Token.deploy('Bowzer Coin','BOWZER','1000000')
+
+    accounts = await ethers.getSigners()
+    deployer= accounts[0]
+    receiver= accounts[1]
+
+
 })
 
 describe('Deployment',()=>{
@@ -40,7 +46,60 @@ it('has correct decimals',async()=>{
 it('has correct total supply',async()=>{
     expect(await token.totalSupply()).to.equal(totalSupply)
 })
+
+it('has correct balance',async()=>{
+    expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 })
 
 
 })
+
+describe('Sending Token', ()=>{
+    let amount,transaction,result
+
+
+describe("Success",()=>{
+
+     beforeEach( async ()=>{
+        amount=tokens(100)
+        transaction= await token.connect(deployer).transfer(receiver.address, amount)
+        result= await transaction.wait()
+
+    })
+
+    it('Transfers token balances', async ()=>{
+        expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+        expect(await token.balanceOf(receiver.address)).to.equal(amount)
+
+      })
+    it('Emits a transfer event', async ()=>{
+        let event=result.events[0]
+        console.log(event)
+        expect(event.event).to.equal('Transfer')
+        let args=event.args
+        expect(args.to).to.equal(receiver.address)
+        expect(args.from).to.equal(deployer.address)
+        expect(args.value).to.equal(amount)
+        
+    })
+
+describe("Failure",()=>{
+    it("rejects insufficient funds", async ()=>{
+        const invalidAmount= tokens(10000000)
+        expect(token.connect(deployer).transfer(receiver.address,invalidAmount)).to.be.reverted
+    })
+
+    it("rejects sending to invalid address", async ()=>{
+        amount=tokens(10)
+        await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000',amount)).to.be.reverted
+    })
+})
+        
+    })
+
+
+
+    
+})
+})
+
