6f69b78c2ed491552b0f7749fee0018e84f3ff77
Bowen Zhang <bowenzhang@Bowens-MacBook-Pro.local>
2022-11-15 23:16:49 -0500

    8. Create exchange

---
+++ contracts/Exchange.sol
@@ -0,0 +1,17 @@
+//SPDX_License_Identifer: unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Exchange {
+   address public feeAccount;
+   uint256 public feePercent;
+
+   constructor(address _feeAccount, uint256 _feePercent) {
+      feeAccount= _feeAccount;
+      feePercent= _feePercent;
+   }
+
+
+
+}
