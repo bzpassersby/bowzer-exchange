6e90965097519cadafb22d89807a5ba1dcd21070
Bowen Zhang <bowenzhang@Bowens-MacBook-Pro.local>
2022-11-19 10:38:14 -0500

    9. Deposit tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -21,7 +21,6 @@ contract Exchange {
 // DEPOSIT & WITHDRAW TOKEN
 function depositToken(address _token, uint256 _value)
 public
-returns (bool success)
 { //deposit token
 
    require(Token(_token).transferFrom(msg.sender, address(this), _value), 'transfer failed');
@@ -34,8 +33,10 @@ returns (bool success)
 //emit deposit event
 emit Deposit(_token, msg.sender, _value, tokens[_token][msg.sender]);
 
-return true;
 }
+
+
+
 
 function balanceOf(address _token, address _user)
 public
